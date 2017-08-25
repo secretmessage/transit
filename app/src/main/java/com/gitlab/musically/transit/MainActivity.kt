6a5a445c40199b03e9e5206b24f6d7cd119030f4
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-25 11:37:35 -0400

    create a file in internal storage using the words hello world

--- app/src/main/java/com/gitlab/musically/transit/MainActivity.kt
+++ app/src/main/java/com/gitlab/musically/transit/MainActivity.kt
@@ -8,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val FILENAME = "hello_file"
+        val string = "hello world!"
+        val fos = openFileOutput(FILENAME, android.content.Context.MODE_PRIVATE)
+        fos.write(string.toByteArray())
+        fos.close()
     }
 }
